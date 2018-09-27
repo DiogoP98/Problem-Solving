52211e4627d279a34158a7907df9c390f5e37af6
Diogo Pereira <diogo99p@gmail.com>
2018-09-27 01:27:25 +0100

    problem 1 done and problem 2 in progress

---
+++ A2/Alphabet.cpp
@@ -0,0 +1,28 @@
+//Problem number 2
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main () {
+    unordered_map<char,int> v;
+
+    int c;
+    char a;
+    int np;
+    int nl;
+
+    cin >> c;
+
+    for (int i=0; i < c; i++) {
+        cin >> a;
+
+        if (a == '?')
+            np++;
+        else {
+            if (v.find(a) == v.end())
+                 
+        }
+    }
+    
+}
---
+++ A2/Omar.cpp
@@ -0,0 +1,18 @@
+//Problem number 1
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main () {
+    int a,b,c;
+    cin >> c;
+    
+    for (int i=0; i < c; i++) {
+        cin >> a >> b;
+
+        int sum = a+b;
+
+        cout << sum << '\n';
+    }
+}
