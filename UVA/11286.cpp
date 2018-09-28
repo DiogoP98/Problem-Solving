eb6e8c6006665765c9e8a6f71e848199428d8ad3
Diogo Pereira <diogo99p@gmail.com>
2018-09-28 21:30:24 +0100

    new problems

---
+++ UVA/11286.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+
+using namespace std;    
+
+map<string,int> m;
+
+int n;
+
+int main () {
+    cin >> n;
+
+    while (n != 0) {
+        map<string,int> m;
+        int count = 1;
+        int max = 0;
+        for (int i=0;i<n;i++) {
+            string a;
+            vector<int> x(5);
+            for (int j=0; j<5; j++) 
+                cin >> x[j];
+            
+            sort(x.begin(), x.end());
+            
+            for (int j=0; j < 5; j++) {
+                a += to_string(x[j]) + " ";
+            }
+            map<string, int>::iterator it = m.find(a);
+
+            if (it != m.end()) 
+                it->second++;
+            else
+                m.insert(pair<string,int>(a,1));
+
+        }
+
+        map<string, int>::iterator it = m.begin();
+
+        for (; it!=m.end(); ++it) {
+            if (it->second > max){
+                count = 1;
+                max= it->second;
+            }
+            else if (it->second == max)
+                count++;
+        }
+        
+        if (max != 1)
+            cout << max*count << endl;
+        else 
+            cout << n << endl;
+
+        cin >> n;
+    }
+
+    return 0;
+}
